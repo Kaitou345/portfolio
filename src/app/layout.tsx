0ec0d74d797bff22065e847fe46c05fc76b0a0ca
seyan <kaitouj345@gmail.com>
2022-12-16 15:25:31 +0600

    Even more Seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,13 +11,14 @@ export default function RootLayout({
       <head>
         <title>Muhammad Seyan</title>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="author" content="Muhammad Seyan" />
         <meta
           name="description"
           content="Have a look around my life. Let's get some work done in the process"
         />
         <meta
           name="keywords"
-          content="programming, coding, web development, react, redux, javascript, html, css, typescript, python, data science, machine learning, ai"
+          content="Muhammad Seyan, Seyan, programming, coding, web development, react, redux, javascript, html, css, typescript, python, data science, machine learning, ai, C, C++, C#"
         />
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </head>
